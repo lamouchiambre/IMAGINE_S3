e4ed938cf4c1ece8619962b400a3f342ee5e5c92
Lamouchi Ambre <lamouchiambre@gmail.com>
2022-10-26 12:50:43 +0200

    debut tuto

---
+++ testOpenGL/test1/main.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+
+#include <algorithm>
+#include <GL/glut.h>
+#include <GLFW/glfw3.h>
+
+
+
+using namespace std;
+
+
+int main (int argc, char ** argv) {
+
+    printf("hello\n");
+
+    return EXIT_SUCCESS;
+}
+
